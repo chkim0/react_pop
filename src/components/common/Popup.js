20e7ab37559fbf37bf458578efa33fe73fe3a89e
chkim0 <kpes2002@naver.com>
2022-10-27 17:52:55 +0900

    메인페이지 수정

--- src/components/common/Popup.js
+++ src/components/common/Popup.js
@@ -1,47 +1,25 @@
-import { forwardRef, useEffect, useImperativeHandle, useState} from "react";
+import { useEffect } from "react";
 
 //팝업 컴포넌트 생성 props으로 setOpen 변수 가져옴
-const Popup = forwardRef((props, ref)=> {
-
-
-    const [Open, setOpen] = useState(false);
-    useImperativeHandle(ref,()=>{
-        return{
-            open: () => setOpen(true),
-
-        };
-
-    
-    });
-
-     useEffect(() => {
-        Open ? (document.body.style.overflow = "hidden")
-            : (document.body.style.overflow = "auto");
-    }, [Open]);
-    //윹튜브 버전
-    // useEffect(() => {
-    //     // 팝업이뜨면
-    //     document.body.style.overflow = "hidden";
-    //     // 사라지면
-    //     return () => {
-    //         document.body.style.overflow = "auto";
-    //     }
-    // }, []);
+function Popup(props) {
+    useEffect(() => {
+        // 팝업이뜨면
+        document.body.style.overflow = "hidden";
+        // 사라지면
+        return () => {
+            document.body.style.overflow = "auto";
+        }
+    }, []);
 
 
     return (
-
-        <>
-        {Open &&(
         // aside pop 호출
         <aside className="pop">
             {/* iframe 값 가져옴 */}
             <div className="con">{props.children}</div>
             {/* 닫기 누르면 open값을 falsefh */}
-            <span className="close" onClick={() => setOpen(false)}>close</span>
+            <span className="close" onClick={() => { props.setOpen(false) }}>close</span>
         </aside>
-        )}
-        </>
     );
-});
+}
 export default Popup;
--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,5 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+
 import Anime from "../../asset/Anime"
-import React, { useRef } from "react"
+import { useRef } from "react"
 
 
 // const btn = {
@@ -11,34 +15,29 @@ const path = process.env.PUBLIC_URL;
 
 export default function Visual() {
 
-    const box = useRef(null);
+  const box = useRef(null);
 
 
-    return (
-        <figure id="visual" className="myScroll">
-                <div className="inner">
-                     <video src={`${path}/img/main_vid.mp4`}
-                     autoplay loop />
-                     <h1>We are making <br/>
-                        real life. 
-                    </h1>
-                    <a href="#">read more about us</a>
-                    
-                    <div className="bbox"></div>
-                    </div>
-                
-          
-            {/* <button
-              style ={btn}
-              onClick={()=>{
-                new Anime(window,{
-                    prop: 'scroll',
-                    value : 1000,
-                    duration : 500,
-                    
-                })
-              }} >button</button> */}
-            <div className='box' ref={box}></div>
-        </figure>
-    )
-}
+  return (
+    <figure id="visual" className="myScroll">
+      <div className="inner">
+        <video src={process.env.PUBLIC_URL + '/img/vid.mp4'} loop autoPlay muted></video>
+        <p className='since'>S I N C E 1 9 9 8</p>
+        <h1>We are making <br />
+          real life.
+        </h1>
+        <a href="#">read more about us</a>
+        <span className='scrd'>S C R O L L   D O W N </span>
+        <div className="bbox"></div>
+        <div className='box' ref={box}></div>
+        <pic className='pic' src={process.env.PUBLIC_URL+ '/img/mainp.jpg'}></pic>
+        <div className='lorem'>
+          <h1>L A S T V I D E O</h1>
+          <p>Wild Iceland in the winter in 10 days</p>
+          <span>Duis tincidunt vestibulum urna, in tempor nibh fermentum sed duis tincidurn massa at facibus.</span>
+         </div>
+        </div >
+    </figure>
+   
+  )
+};
